9dca317e79319cb6bcd7f285fd181ef4e26dfed2
Ragesh M <mragesh1998@gmail.com>
2021-07-04 10:15:55 +0530

    sortArray() method updated

--- Task-1/ArraySorter.java
+++ Task-1/ArraySorter.java
@@ -1,14 +1,18 @@
 
 public class ArraySorter {
 	public static void sortArray(Integer[] arr) {
+		boolean swapped;
 		for(int i = 0; i < arr.length-1; i++) {
-			for(int j = i; j < arr.length; j++) {
+			swapped = false;
+			for(int j = i+1; j < arr.length; j++) {
 				if(arr[i] > arr[j]) {
 					int temp = arr[i];
 					arr[i] = arr[j];
 					arr[j] = temp;
+					swapped = true;
 				}
 			}
+			if(swapped == false) break;
 		}
 		System.out.println("Sorted Array: " + Arrays.asList(arr));
 	}
